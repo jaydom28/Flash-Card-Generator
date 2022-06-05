d8af5897af4f60ad597a5e7d755be1f65f693167
Justin Dom <jaydom28@gmail.com>
2022-06-05 17:19:11 -0400

    refactored dictionary classes

--- CambridgeDictionary.py
+++ CambridgeDictionary.py
@@ -12,30 +12,32 @@ class CambridgeDictionaryScraper:
     def _get_html(self, word: str) -> str:
         """
         Scrape the webpage of a specific word from the dictionary website and
-        return the html
+        return the html as a string
         """
         url = f'{self.base_url}/{word}'
 
         res = requests.get(url, headers={"User-Agent": "XY"})
         if not res.ok:
-            print(f'Unable to scrape the website for: {word}')
+            print(f'Unable to scrape Cambridge Dictionary for: {word}')
             return ''
 
         return res.content
 
     def _get_definitions(self, web_page_html: str) -> list[(str, str)]:
         """
-        Takes in HTML representing a word definition and parses out the definitions
-        Returns a list of tuples in the form: (part of speech, translation)
+        Takes in HTML representing the web page of a word from the cambridge
+        dictionary website and parses out the translations
+        Returns a list of tuples in the form:
+            (part of speech, translation)
         """
         web_page_soup = BeautifulSoup(web_page_html, 'lxml')
-        definition_tags = web_page_soup.find_all('div', attrs={'class': self.language})
+        translation_soups = web_page_soup.find_all('div', attrs={'class': self.language})
         output = []
 
-        for definition in definition_tags:
-            part_of_speech = definition.find('span', attrs={'class': 'pos'}).text
-            translation = definition.find('span', attrs={'class': 'trans dtrans'}).text
-            output.append((part_of_speech, translation))
+        for soup in translation_soups:
+            part_of_speech = soup.find('span', attrs={'class': 'pos'}).text
+            word = soup.find('span', attrs={'class': 'trans dtrans'}).text
+            output.append((part_of_speech, word))
         
         return output
 
@@ -59,6 +61,7 @@ class GermanEnglishDictionary(CambridgeDictionaryScraper):
     def __init__(self):
         super().__init__('german-english')
 
+
 def get_dictionary(from_language: str, to_language: str) -> CambridgeDictionaryScraper:
     """
     Takes in two languages and returns the correct dictionary to be used
@@ -73,5 +76,3 @@ def get_dictionary(from_language: str, to_language: str) -> CambridgeDictionaryS
 
     print('There is no currently implemented dictionary object with the type: {from_language} -> {to_language}') 
     return None
-
-
--- main.py
+++ main.py
@@ -1,23 +1,55 @@
+import os
 import requests
 import sys
-
-from bs4 import BeautifulSoup
 
 from CambridgeDictionary import get_dictionary
 
 
-english_german_dictionary = get_dictionary('en', 'de')
-german_english_dictionary = get_dictionary('de', 'en')
-
-words = sys.argv[1:]
-
-for word in words:
-    print('Here are translations for: {word}')
-    for (pos, translation) in english_german_dictionary.get_word(word):
-        print(f'{pos}: {translation}')
+def get_word_of_string(string: str) -> str:
+    """
+    Takes in a string and returns the first word of the string
+    """
+    return string.split()[0]
 
 
-# STEP 1: Get the words to translate
-# STEP 2: Scrape the definitions of all the words
-# STEP 3: Format the definitions in a CSV file
-# Would be cool to get pictures too for the definitions
+def usage() -> None:
+    script_name = os.path.basename(__file__)
+    print('USAGE:')
+    print(f'\tpython {script_name} [WORDS]')
+    sys.exit(1)
+
+
+def word_loop() -> None:
+    en_de_dictionary = get_dictionary('en', 'de')
+    de_en_dictionary = get_dictionary('de', 'en')
+    for line in sys.stdin:
+        word = get_word_of_string(line)
+        translations = de_en_dictionary.get_word(word) or en_de_dictionary.get_word(word)
+        if not translations:
+            print(f'Unable to get definition for: {word}')
+            continue
+        
+        for (pos, translation) in translations:
+            print(f'{pos} -> {translation}')
+
+
+def main():
+    # STEP 1: Get the words to translate
+    if len(sys.argv) <= 2:
+        word_loop()
+        sys.exit(0)
+
+    english_german_dictionary = get_dictionary('en', 'de')
+    german_english_dictionary = get_dictionary('de', 'en')
+    words_file = sys.argv[1]
+
+    # STEP 2: Scrape the definitions of all the words
+
+    # STEP 3: Format the definitions in a CSV file
+
+    # Would be cool to get pictures too for the definitions
+    print('AYO')
+
+if __name__ == '__main__':
+    main()
+
