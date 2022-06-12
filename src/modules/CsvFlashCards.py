b819ae698ce221dc8bdb65766e77a280330fd76c
Justin <jaydom28@gmail.com>
2022-06-11 22:08:11 -0400

    Added docstrings to CsvFlashCards

--- src/modules/CsvFlashCards.py
+++ src/modules/CsvFlashCards.py
@@ -15,6 +15,9 @@ TAGS_FIELD = '{{Tags}}'
 
 
 def get_examples_string(examples: Iterator[str]) -> str:
+    """
+    Creates a string that represents a sequence of examples that can be imported into Anki from a CSV file
+    """
     examples_list = list(examples)
     if not examples_list:
         return ''
@@ -26,15 +29,24 @@ def get_examples_string(examples: Iterator[str]) -> str:
 
 
 def get_tags_string(tags: List[str], delim=' ') -> str:
+    """
+    Takes in a list of tags and formats it into a string which can be imported into Anki from a CSV file
+    """
     return delim.join(tags)
 
 
 def get_noun_plural(translation: Dict) -> str:
+    """
+    Takes in a dictionary representing a German noun from Cambridge Dictionary and returns the plural form of the word
+    """
     return next((word for (case, p_form, word, *_) in translation['forms']
                 if (case, p_form) == ('nominative', 'plural')), '')
 
 
 def create_noun_flashcard(translation: Dict) -> List[Tuple[str, str, str]]:
+    """
+    Takes in a dictionary representing German noun and exports a tuple representing sides of a flash card
+    """
     pos = translation['pos']
     front_side = f'{translation["english_translation"]} (en)'
 
@@ -54,6 +66,9 @@ def create_noun_flashcard(translation: Dict) -> List[Tuple[str, str, str]]:
 
 
 def create_verb_flashcard(translation: Dict) -> List[Tuple[str, str, str]]:
+    """
+    Takes in a dictionary representing German verb and exports a tuple representing sides of a flash card
+    """
     front_side = f'{translation["english_translation"]} (en)'
     back_side = translation['word'] + '\n'
 
@@ -78,6 +93,9 @@ def create_verb_flashcard(translation: Dict) -> List[Tuple[str, str, str]]:
 
 
 def create_adjective_flashcard(translation: Dict) -> List[Tuple[str, str, str]]:
+    """
+    Takes in a dictionary representing a German adjective and exports a tuple representing sides of a flash card
+    """
     front_side = f'{translation["english_translation"]} (en)'
     back_side = translation['word'] + '\n'
 
@@ -93,6 +111,9 @@ def create_adjective_flashcard(translation: Dict) -> List[Tuple[str, str, str]]:
 
 
 def create_adverb_flashcard(translation: Dict) -> List[Tuple[str, str, str]]:
+    """
+    Takes in a dictionary representing a German adverb and exports a tuple representing sides of a flash card
+    """
     front_side = f'{translation["english_translation"]} (en)'
 
     back_side = translation['word'] + '\n'
@@ -106,6 +127,11 @@ def create_adverb_flashcard(translation: Dict) -> List[Tuple[str, str, str]]:
 
 
 def create_flashcard_tuple(translation: Dict) -> List[Tuple[str, str, str]]:
+    """
+    Takes in a dictionary representing a German translation and creates a tuple representing sides of a flash card
+    Returns the tuple in the form:
+        (front side, back side, tags)
+    """
     pos = translation['pos']
     
     if pos == 'noun':
@@ -122,6 +148,9 @@ def create_flashcard_tuple(translation: Dict) -> List[Tuple[str, str, str]]:
 
 
 def card_tuples_to_csv(card_tuples: Iterator[Tuple[str, str, str]], delim=',') -> str:
+    """
+    Takes in an iterable sequence of tuples representing sides of a flash card and outputs a CSV representation of the tuple
+    """
     output = ''
 
     for card_tuple in card_tuples:
